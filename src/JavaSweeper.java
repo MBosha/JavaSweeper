1b030c9e0ddb649b6e9eaac2c6107cb3593adea6
MBosha <boshyn@gmail.com>
2019-04-04 23:45:38 +0300

    2019-04-04 23:45
    2019-04-04 23:45

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,22 +1,68 @@
+import sweeper.Box;
+import java.awt.*;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-
-public class JavaSweeper extends JFrame{
+public class JavaSweeper extends JFrame
+{
+    private final int COL = 15;
+    private final int ROW = 1;
+    private final int IMAGE_SIZE = 50;
+    private JPanel panel;
 
     public static void main(String[] args) 
     {
         new JavaSweeper().setVisible(true);
     }
     
-    public JavaSweeper()
+    private JavaSweeper()            
     {
+        setImages();
+        initPanel();
+        initFrame();
+    }
+    
+    private void initPanel() 
+    {
+        panel = new JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) 
+            {
+                super.paintComponent(g);
+                for (Box  box : Box.values()) 
+                {
+                    g.drawImage((Image)box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                }
+            }                   
+        };
+        panel.setPreferredSize(new Dimension(COL * IMAGE_SIZE, ROW * IMAGE_SIZE));
+        add(panel);
+    };
+    
+    private void setImages()
+    {
+        for (Box  box : Box.values()) 
+        {
+            box.image = getImage(box.name().toLowerCase());
+        }
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Jawa Sweeper");
-        setLocationRelativeTo(null);
+        setTitle("Jawa Sweeper");        
         setVisible(true);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
+    
+    private Image getImage(String name)
+    {        
+        ImageIcon icon = new ImageIcon("res\\img\\" + name + ".png");
+        return icon.getImage();
+    };
+
+
             
     
 }
