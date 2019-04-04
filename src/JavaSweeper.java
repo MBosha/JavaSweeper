2c3aaa6882be244d26a67d414f286aa3a440ceab
MBosha <boshyn@gmail.com>
2019-04-04 16:55:49 +0300

    Begin
    Begin

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,10 +1,22 @@
-public class JavaSweeper {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class JavaSweeper extends JFrame{
+
+    public static void main(String[] args) 
+    {
+        new JavaSweeper().setVisible(true);
     }
+    
+    public JavaSweeper()
+    {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Jawa Sweeper");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+            
     
 }
